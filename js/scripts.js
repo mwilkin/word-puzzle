e5f2426a906ec68ac1b4a36ef6757ce1302b34dd
Matt Wilkin and Rachael Bermudes <matt@>
2016-06-07 16:37:46 -0700

    updated jquery scripts.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
-  $("#blanks form").submit(function(event)
-  
+  $("form#questions").submit(function(event)
+     
+    var input = $("input#name").val();
+    var result = $(puzzle)
+    var vowels = ["a", "e", "i", "o", "u"];
+    ("-") input.replaceAll (vowels);
+    $("#puzzle").text (result);
